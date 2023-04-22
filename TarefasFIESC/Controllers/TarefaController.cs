b6479395fb5f8ba134f402a8afb7d0e984c47cea
Joaonorr <joaonoroes.jn@gmail.com>
2023-04-22 18:04:52 -0300

    feat: Criando controlador de Tarefas com a funcionalidade de Listar tarefas junto com Gerenciador de sessão

---
+++ TarefasFIESC/Controllers/TarefaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TarefasFIESC.Repository;
+using TarefasFIESC.Sessoes;
+
+namespace TarefasFIESC.Controllers;
+
+public class TarefaController : Controller
+{
+
+    private readonly ITarefaRepository _tarefaRepository;
+    private readonly IObservacaoRepository _observacaoRepository;
+    private readonly IConfiguration _configuration;
+    private readonly ISessao _sessao;
+
+    public TarefaController(ITarefaRepository tarefaRepository,
+        IObservacaoRepository observacaoRepository,
+        ISessao sessao,
+        IConfiguration configuration)
+    {
+        _tarefaRepository = tarefaRepository;
+        _observacaoRepository = observacaoRepository;
+        _sessao = sessao;
+        _configuration = configuration;
+    }
+}
---
+++ TarefasFIESC/Helpers/GerenciadorDeSessao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using TarefasFIESC.Models;
+using TarefasFIESC.Repository;
+using TarefasFIESC.Seguranca;
+using TarefasFIESC.Sessoes;
+
+namespace TarefasFIESC.Helpers;
+
+public class GerenciadorDeSessao
+{
+    private readonly ISessao _sessao;
+
+    public GerenciadorDeSessao(ISessao sessao)
+    {
+        _sessao = sessao;            
+    }
+
+    public UsuarioModel ResgatarNomeDoUsuario()
+    {
+        var sessao = _sessao.BuscarSessao();
+
+        var usuario = GerenciarToken.DescrToken(sessao);
+
+        return new UsuarioModel
+        {
+            Nome = usuario.Nome
+        };
+    }
+}
