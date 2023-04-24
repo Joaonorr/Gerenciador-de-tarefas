4e14428f1389fff7f61a289eee51b83d7e39115b
Joaonorr <joaonoroes.jn@gmail.com>
2023-04-24 04:14:06 -0300

    refactor: separando os middlewares em arquivo separado

---
+++ TarefasFIESC/Middleware/Middleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TarefasFIESC.Data;
+
+namespace TarefasFIESC.Middleware;
+
+public static class Middleware
+{
+    public static void ConfiguracaoMiddleware(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        Console.WriteLine("Applying migrations");
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+        }
+
+        Console.WriteLine("Done");
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        // session enable
+        app.UseSession();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Login}/{action=Entrar}/{id?}");
+
+    }
+}
--- TarefasFIESC/Program.cs
+++ TarefasFIESC/Program.cs
@@ -1,3 +1,4 @@
+using TarefasFIESC.Middleware;
 using TarefasFIESC.Servicos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,27 +7,6 @@ builder.ConfiguracaoServico();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-// session enable
-app.UseSession();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Login}/{action=Entrar}/{id?}");
+app.ConfiguracaoMiddleware();
 
 app.Run();
